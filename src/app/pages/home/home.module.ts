b6b384b2f32e535803e22eff23678413fb6d80c1
Wellington Batista <wellingtonbs109@gmail.com>
2023-09-11 12:48:54 -0300

    feat: created component header

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,6 +6,8 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+//components
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
   imports: [
@@ -14,6 +16,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    HeaderComponent]
 })
 export class HomePageModule {}
